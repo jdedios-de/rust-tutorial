63df493f445e2aa8cb9fe063caf94b49336dcae8
Jerome de Dios <jeromededios.de@proton.me>
2025-06-11 22:28:40 +0200

    feat: adds data type

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,8 @@
 fn main() {
-   const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    let guess: u32 = "42".parse().expect("Not a number!");
 
-   let mut x = 5;
+    let temp: f32 = "34.5".parse().expect("Not a number!");
 
-   println!("The value of x is: {x}");
-
-   x = 6;
-
-   println!("The value of x is: {x}");
-
-   println!("{}", THREE_HOURS_IN_SECONDS);
-
-
-   let y = 5;
-
-   let y = y + 1;
-
-   {
-      let y = y * 2;
-      println!("The value of x in the inner scope is: {y}");
-   }
-
-   println!("The value of x is: {y}");
-
-
-   let spaces = "    ";
-   let spaces = spaces.len();
-   
-   println!("{}", spaces)
+    println!("The guess is {}", guess);
+    println!("The temp is {}", temp);
 }
