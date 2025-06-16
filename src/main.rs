de6a8608de49a2584378f0ccf588e280a7a8463c
Jerome de Dios <jeromededios.de@proton.me>
2025-06-16 16:05:28 +0200

    feat: adds Using if in a let Statement

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 fn main() {
-    let number: i32 = 11;
+    let condition: bool = true;
     
-    if number % 4 == 0 {
-        println!("Number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("Number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("Number is divisible by 2");
-    } else {
-        println!("Number is not divisible by 4, 3, or 2");   
-    }
+    let number : i32 = if condition { 5 } else { 6 };
+    
+    println!("{}", number);
 }
