6fb6d19fa92f63a407525053ed8f2ce970168637
Jerome de Dios <jeromededios.de@proton.me>
2025-06-16 18:37:44 +0200

    feat: adds Looping Through a Collection with for

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 fn main() {
-    let mut number: i32 = 3;
+    let a:[i32;5] = [10, 20, 30, 40, 50];
 
-    while number != 0 {
-        println!("{}", number);
-        number -= 1;
+    let mut index : usize = 0;
+
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index += 1;
     }
 
-    println!("Blastoff!");
+    for i in a {
+        println!(" The value is : {}", i)
+    }
+
+    for number in (1..4).rev(){
+        println!("{number}")
+    }
 }
