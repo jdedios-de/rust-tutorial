f827142b4a8d991f67c68c8555e2677de5b6ece9
Jerome de Dios <jeromededios.de@proton.me>
2025-06-16 16:02:34 +0200

    feat: control flow if else

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-
-// Functions that returns value
-
-
-fn plus_one(x: i32) -> i32 {
-    x + 1
-}
 fn main() {
-    let x: i32 = plus_one(5);
+    let number: i32 = 11;
     
-    println!("x is {}", x);
+    if number % 4 == 0 {
+        println!("Number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("Number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("Number is divisible by 2");
+    } else {
+        println!("Number is not divisible by 4, 3, or 2");   
+    }
 }
